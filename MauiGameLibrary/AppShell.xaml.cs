cf59ab031c825d73dd139153559ac45d5d2053e4
Allan Pead <adpead@gmail.com>
2025-07-29 08:01:32 +0200

    Add project files.

---
+++ MauiGameLibrary/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MauiGameLibrary
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+           Routing.RegisterRoute("updategameroute", typeof(Views.UpdateGameView));
+        }
+    }
+}
